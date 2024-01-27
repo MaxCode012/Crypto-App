9fc81fac2a73a5f4954b5315163ea34bef9fe26f
Seremet Maxim <seremet.maxim94@gmail.com>
2024-01-27 19:27:19 +0200

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { ThemeProvider } from "./context/ThemeContex";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div></div>;
+  return (
+    <ThemeProvider>
+      <Navbar />
+    </ThemeProvider>
+  );
 }
 
 export default App;
