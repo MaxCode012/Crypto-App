d3b2a737412c02e79248162757e4fc071ecf4fdf
Seremet Maxim <seremet.maxim94@gmail.com>
2024-01-28 14:46:35 +0200

    added Trending Coins and dynamic Coin Page for each coin

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import SignUp from "./routes/SignUp";
 import Account from "./routes/Account";
 import SignIn from "./routes/SignIn";
 import axios from "axios";
+import CoinPage from "./routes/CoinPage";
 
 function App() {
   const [coins, setCoins] = useState([]);
@@ -29,6 +30,9 @@ function App() {
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/account" element={<Account />} />
+        <Route path="/coin/:coinId" element={<CoinPage />}>
+          <Route path=":coinId" />
+        </Route>
       </Routes>
     </ThemeProvider>
   );
